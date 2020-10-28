af8b0337a0ccef28a5c26aa6f33bed38bb248532
EugeneKHL <jenyhlevnoy@gmail.com>
2020-10-28 19:47:26 +0300

    First project commit

---
+++ src/ru/ioffe/school/susanin/Main.java
@@ -0,0 +1,12 @@
+package ru.ioffe.school.susanin;
+
+import java.util.Scanner;
+import java.io.File;
+import java.io.IOException;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+}
