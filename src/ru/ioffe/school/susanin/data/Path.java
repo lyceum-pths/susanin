84c39783c0dad1fe3208d382186126173dbd96a4
MedVAL-na <72570836+medval-na@users.noreply.github.com>
2020-11-06 17:27:07 +0300

    changed speed
    Signed-off-by: MedVAL-na <72570836+MedVAL-na@users.noreply.github.com>

--- src/ru/ioffe/school/susanin/data/Path.java
+++ src/ru/ioffe/school/susanin/data/Path.java
@@ -2,7 +2,7 @@ package ru.ioffe.school.susanin.data;
 
 public class Path extends Way {
 
-    private static final int walkSpeed = 10;
+    private static final int walkSpeed = 5;
 
     public Path(double length) {
         super(length, walkSpeed, false);
