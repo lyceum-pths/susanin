74d410eae0259f5244e9c11f43685f1727423672
EugeneKHL <jenyhlevnoy@gmail.com>
2021-02-22 14:52:06 +0300

    Added fields and methods to vertex and edge classes

--- src/ru/ioffe/school/susanin/mapGraph/Edge.java
+++ src/ru/ioffe/school/susanin/mapGraph/Edge.java
@@ -4,4 +4,41 @@ package ru.ioffe.school.susanin.mapGraph;
  * Represents a graph edge
  */
 public class Edge {
+
+    private final Vertex from;
+    private final Vertex to;
+    private final int time;
+    private final int cost;
+
+    /**
+     * Constructs Edge with specific cost and
+     * time required to go through it.
+     *
+     * @param from edge start
+     * @param to   edge end
+     * @param time time in minutes required to go through edge
+     * @param cost edge cost
+     */
+    public Edge(Vertex from, Vertex to, int time, int cost) {
+        this.from = from;
+        this.to = to;
+        this.time = time;
+        this.cost = cost;
+    }
+
+    public Vertex getFrom() {
+        return from;
+    }
+
+    public Vertex getTo() {
+        return to;
+    }
+
+    public int getCost() {
+        return cost;
+    }
+
+    public int getTime() {
+        return time;
+    }
 }
--- src/ru/ioffe/school/susanin/mapGraph/Vertex.java
+++ src/ru/ioffe/school/susanin/mapGraph/Vertex.java
@@ -1,7 +1,31 @@
 package ru.ioffe.school.susanin.mapGraph;
 
+import ru.ioffe.school.susanin.data.Point;
+
 /**
  * Represents a graph vertex
  */
 public class Vertex {
+
+    private final Point ref;
+    private final int id;
+
+    /**
+     * Constructs Vertex connected to specific point on map.
+     *
+     * @param ref reference to a point on a map
+     * @param id  unique number in graph
+     */
+    public Vertex(Point ref, int id) {
+        this.ref = ref;
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public Point getRef() {
+        return ref;
+    }
 }
