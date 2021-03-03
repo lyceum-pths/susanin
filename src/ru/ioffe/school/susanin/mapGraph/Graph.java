82924ea371e68351e68876b30259add5b42a4091
MedVAL-na <72570836+MedVAL-na@users.noreply.github.com>
2021-03-03 18:53:51 +0300

    Added maps for sequences, departure and arrival

--- src/ru/ioffe/school/susanin/mapGraph/Graph.java
+++ src/ru/ioffe/school/susanin/mapGraph/Graph.java
@@ -1,7 +1,21 @@
 package ru.ioffe.school.susanin.mapGraph;
+import java.io.IOException;
+import java.util.ArrayList;
 
 /**
  * Represents a map with a graph.
  */
 public class Graph {
+    public  Graph(ArrayList<Vertex> vertexes, ArrayList<Edge> edges) {
+
+        ArrayList<ArrayList<Edge> > graph = new  ArrayList< ArrayList<Edge>>(1);
+
+            for (edge: edges){
+               Vertex from = Edge.getFrom() ;
+               graph.add() = Edge.getTo()
+            }
+
+
+
+    }
 }
