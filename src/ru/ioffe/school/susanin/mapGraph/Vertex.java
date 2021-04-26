cdbe02b2fdd3c02f4794e30bbcf8d5f2cc0e8383
EugeneKHL <jenyhlevnoy@gmail.com>
2021-04-27 00:05:23 +0300

    Added priority heap Dijkstra algorithm

--- src/ru/ioffe/school/susanin/mapGraph/Vertex.java
+++ src/ru/ioffe/school/susanin/mapGraph/Vertex.java
@@ -2,13 +2,18 @@ package ru.ioffe.school.susanin.mapGraph;
 
 import ru.ioffe.school.susanin.data.Point;
 
+import java.util.Comparator;
+
 /**
  * Represents a graph vertex
  */
-public class Vertex {
+public class Vertex implements Comparator<Vertex> {
+
+    private static final double infinity = 1000000.0;
 
     private final Point ref;
     private final int id;
+    private double time;
 
     /**
      * Constructs Vertex connected to specific point on map.
@@ -19,6 +24,11 @@ public class Vertex {
     public Vertex(Point ref, int id) {
         this.ref = ref;
         this.id = id;
+        this.time = infinity;
+    }
+
+    public void setTime(double time) {
+        this.time = time;
     }
 
     public int getId() {
@@ -27,5 +37,20 @@ public class Vertex {
 
     public Point getRef() {
         return ref;
+    }
+
+    public double getTime() {
+        return time;
+    }
+
+    @Override
+    public int compare(Vertex v1, Vertex v2) {
+        if (v1.time > v2.time) {
+            return 1;
+        }
+        if (v1.time < v2.time) {
+            return -1;
+        }
+        return 0;
     }
 }
--- src/ru/ioffe/school/susanin/navigator/Navigator.java
+++ src/ru/ioffe/school/susanin/navigator/Navigator.java
@@ -4,17 +4,16 @@ import ru.ioffe.school.susanin.data.Point;
 import ru.ioffe.school.susanin.mapGraph.Edge;
 import ru.ioffe.school.susanin.mapGraph.Vertex;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
+import java.util.*;
 
 public class Navigator {
 
-    public Navigator() {};
+    public Navigator() {
+    }
 
     public double findPathSquare(Point start, Point end, int maxCost, HashMap<Vertex, ArrayList<Edge>> graph,
-                           ArrayList<Vertex> vertices) {
-        final int INFINITY = 1000;
+                                 ArrayList<Vertex> vertices) {
+        final int INFINITY = 100000;
         double[] times = new double[vertices.size()];
         Arrays.fill(times, INFINITY);
         boolean[] visited = new boolean[vertices.size()];
@@ -48,5 +47,51 @@ public class Navigator {
             }
         }
         return times[to.getId()];
+    }
+
+    public double findPath(Point start, Point end, int maxCost, HashMap<Vertex, ArrayList<Edge>> graph,
+                           ArrayList<Vertex> vertices) {
+        final double INFINITY = 1000000.0;
+        double[] times = new double[vertices.size()];
+        Arrays.fill(times, INFINITY);
+        Vertex from = vertices.get(0), dest = vertices.get(0);
+        for (Vertex vertex : vertices) {
+            if (vertex.getRef().equals(start)) {
+                from = vertex;
+            } else if (vertex.getRef().equals(end)) {
+                dest = vertex;
+            }
+            vertex.setTime(INFINITY);
+        }
+        times[vertices.indexOf(from)] = 0.0;
+        PriorityQueue<Vertex> queue = new PriorityQueue<>((v1, v2) -> {
+            if (v1.getTime() > v2.getTime()) {
+                return 1;
+            }
+            if (v1.getTime() < v2.getTime()) {
+                return -1;
+            }
+            return 0;
+        });
+        queue.add(from);
+        while (!queue.isEmpty()) {
+            Vertex vertex = queue.poll();
+            double currentTime = vertex.getTime();
+            if (currentTime > times[vertices.indexOf(vertex)]) {
+                continue;
+            }
+
+            for (Edge edge : graph.get(vertex)) {
+                Vertex to = edge.getTo();
+                double gap = edge.getTime(0.0);
+                if (times[vertex.getId()] + gap < times[to.getId()]) {
+                    to.setTime(times[vertex.getId()] + gap);
+                    times[to.getId()] = to.getTime();
+                    // route[to.getId()] = vertex.getId();
+                    queue.add(to);
+                }
+            }
+        }
+        return times[dest.getId()];
     }
 }
